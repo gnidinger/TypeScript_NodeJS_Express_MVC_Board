315120129217dfcdaf3e23e8497b5fd8a502fd59
gnidinger <gcnb87@gmail.com>
2023-07-01 21:51:03 +0900

    feat: createFeed, getAllFeeds

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import feedRoutes from './routes/feedRoutes';
+import morgan from 'morgan';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.use(express.json);
+app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/feeds', feedRoutes);
 
