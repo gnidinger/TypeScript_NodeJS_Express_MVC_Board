d8110d5f8e9beec725e6645f057a66b71aec6f03
gnidinger <gcnb87@gmail.com>
2023-07-05 07:35:47 +0900

    auto increment seq

--- src/models/Feed.ts
+++ src/models/Feed.ts
@@ -1,23 +1,28 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
 
 interface IFeed extends Document {
-  seq: number;
+  feedSeq: number;
   title: string;
   body: string;
 }
 
-const feedSchema: Schema<IFeed> = new Schema({
-  seq: { type: Number, unique: true, required: true, default: 1 },
-  title: { type: String, required: true },
-  body: { type: String, required: true },
-});
+const feedSchema: Schema<IFeed> = new Schema(
+  {
+    feedSeq: { type: Number, unique: true, required: true, default: 1 },
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 feedSchema.pre('save', async function (next) {
   if (this.isNew) {
     const FeedModel = this.constructor as Model<IFeed>;
-    const lastFeed = await FeedModel.findOne({}, {}, { sort: { seq: -1 } });
+    const lastFeed = await FeedModel.findOne({}, {}, { sort: { feedSeq: -1 } });
     if (lastFeed) {
-      this.seq = lastFeed.seq + 1;
+      this.feedSeq = lastFeed.feedSeq + 1;
     }
   }
   next();
