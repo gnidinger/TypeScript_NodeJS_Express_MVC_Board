34f5118e95175b02e61ae4ff74f20ea1af78b0b5
gnidinger <gcnb87@gmail.com>
2023-07-23 14:17:52 +0900

    CI/CD

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,9 @@
 # curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 # sudo apt-get install -y nodejs
 
+# 애플리케이션 디렉토리 생성 (이미 존재하면 무시됨)
+sudo mkdir -p /home/ubuntu/action
+
 # 애플리케이션 디렉토리로 이동
 cd /home/ubuntu/action
 
