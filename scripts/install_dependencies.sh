7a116ae45b8c445139f37cc278bd90a05183fafc
gnidinger <gcnb87@gmail.com>
2023-07-23 14:24:21 +0900

    CI/CD

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,9 @@
 # curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 # sudo apt-get install -y nodejs
 
+# Add Node.js and npm to PATH
+export PATH="/home/ubuntu/.nvm/versions/node/v18.17.0/bin:$PATH"
+
 # 애플리케이션 디렉토리 생성 (이미 존재하면 무시됨)
 sudo mkdir -p /home/ubuntu/action
 
@@ -11,4 +14,4 @@ sudo mkdir -p /home/ubuntu/action
 cd /home/ubuntu/action
 
 # npm dependencies 설치
-/home/ubuntu/.nvm/versions/node/v18.17.0/bin/npm install
+$NPM_PATH install
