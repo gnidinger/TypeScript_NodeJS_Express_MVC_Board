cbb78ed4c80e7a55a20c1f8fcecffe4e1cacf4d7
gnidinger <gcnb87@gmail.com>
2023-07-23 16:19:56 +0900

    CI/CD

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,3 @@ node_path=$(/home/ubuntu/.nvm/versions/node/v18.17.0/bin/node)
 
 # 애플리케이션 디렉토리로 이동
 cd /home/ubuntu/action
-
-# 애플리케이션 빌드
-$node_path /home/ubuntu/.nvm/versions/node/v18.17.0/bin/npm run build
