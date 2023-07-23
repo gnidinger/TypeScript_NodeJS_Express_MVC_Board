93f9443a9d4da2ff933d242e4fcf39d59da31bc0
gnidinger <gcnb87@gmail.com>
2023-07-23 13:23:55 +0900

    fix: CI/CD

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Node.js 설치
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# # Node.js 설치
+# curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+# sudo apt-get install -y nodejs
 
 # 애플리케이션 디렉토리로 이동
 cd /home/ubuntu/action
